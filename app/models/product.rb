be9f2c6a80474d73b30677db1576ae07dea50c85
Brandon Espinoza <espinozab100@gmail.com>
2018-12-31 06:13:24 -0600

    updating model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,13 @@ class Product < ApplicationRecord
   friendly_id :name, use: :slugged
   # belongs_to :color
 
-  has_attached_file :small_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/missing.png"
+  has_attached_file :small_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :small_img, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :big_img_one, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/missing.png"
+  has_attached_file :big_img_one, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :big_img_one, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :big_img_two, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/missing.png"
+  has_attached_file :big_img_two, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :big_img_two, content_type: /\Aimage\/.*\z/
 
   # PRODUCT_TYPE = [
